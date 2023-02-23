4f8b653c071d8461c8751dd8b0b1176b4ecb5fdf
mohamed kaid <mohamedkaid824@gmail.com>
2023-02-23 05:35:30 -0500

    day 2 hw

---
+++ homeworkdll.sql
@@ -0,0 +1,32 @@
+CREATE TABLE customers(
+    customer_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
+);
+
+CREATE TABLE tickets(
+    tickets_id SERIAL PRIMARY KEY,
+    title VARCHAR(50),
+    description_info VARCHAR(50)
+);
+
+CREATE TABLE concessions(
+    concessions_id SERIAL PRIMARY KEY,
+    food VARCHAR(50),
+    drinks VARCHAR(50)
+);
+
+CREATE TABLE movies(
+    movies_id SERIAL PRIMARY KEY,
+    film VARCHAR(50),
+    category_info VARCHAR(50),
+    customer_id INTEGER,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    tickets_id INTEGER,
+    FOREIGN KEY (tickets_id) REFERENCES tickets(tickets_id),
+    concessions_id INTEGER,
+    FOREIGN KEY (concessions_id) REFERENCES concessions(concessions_id)
+);
+
+ALTER TABLE movies
+ADD customer_id INTEGER;
---
+++ homeworkdml.sql
@@ -0,0 +1,87 @@
+SELECT *
+FROM customers;
+
+
+INSERT INTO customers(
+    first_name,
+    last_name,
+) VALUES (
+    'mohamed',
+    'kaid',
+);
+
+INSERT INTO customers(
+    first_name,
+    last_name,
+) VALUES (
+    'naruto',
+    'uzumaki',
+);
+
+INSERT INTO customers(
+    first_name,
+    last_name,
+) VALUES (
+    'eren',
+    'jaeger',
+);
+
+INSERT INTO tickets(
+    tickets_id,
+    title,
+    description_info,
+) VALUES (
+    'Anime',
+    'DBS Broly',
+    'Action'
+);
+
+INSERT INTO tickets(
+    tickets_id,
+    title,
+    description_info,
+) VALUES (
+    'Anime',
+    'Naruto the last',
+    'adventure'
+);
+
+INSERT INTO tickets(
+    tickets_id,
+    title,
+    description_info,
+) VALUES (
+    'Anime',
+    'My hero academia',
+    'superpowers'
+);
+
+INSERT INTO concessions(
+    concessions_id,
+    food,
+    drinks,
+) VALUES (
+    'food spot',
+    'popcorn',
+    'soda',
+);
+
+INSERT INTO concessions(
+    concessions_id,
+    food,
+    drinks,
+) VALUES (
+    'food spot',
+    'pizza',
+    'juices',
+);
+
+INSERT INTO concessions(
+    concessions_id,
+    food,
+    drinks,
+) VALUES (
+    'food spot',
+    'pretzels',
+    'water',
+);
